b202ceddc0bfb2f1dbf8c1ecf8ebb1c116bc9b13
yingrak34 <yingrak34@hotmail.com>
2017-02-16 10:50:37 +0700

    Update index.php

--- index.php
+++ index.php
@@ -26,7 +26,7 @@ if (!is_null($events['events'])) {
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
 				'replyToken' => $replyToken,
-				'messages' => [$messages],
+				'messages' => [$messages+TESTNAJA],
 			];
 			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
