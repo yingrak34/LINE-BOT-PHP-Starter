676f0a64c6eb97bc72c0c6f720ad17efacec5c43
yingrak34 <yingrak34@hotmail.com>
2017-02-16 08:51:40 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,15 @@
 <?php
-echo "Hello LINE BOT THIS IS MY BOT OR NOT?";
+$access_token = 'YED5raqOz3sYPCZqv3njAKfoDiGoWqpFw1ZUK6UsNUg1Aj3NYjdyOc+HicIrcbvYKZe/itybqX1qGweFfZYupChf1M2MOgBqYHhBFnliDxzow3UoHEulJD1cGOavsAzildgR+gMx/KwNMv9GiWx6pQdB04t89/1O/w1cDnyilFU=';
+
+$url = 'https://api.line.me/v2/oauth/verify';
+
+$headers = array('Authorization: Bearer ' . $access_token);
+
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+$result = curl_exec($ch);
+curl_close($ch);
+
+echo $result;
